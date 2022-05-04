28a0a858c50755eacc5b6e481d3d5b8ccefb9017
MarceloA22 <104115950+MarceloA22@users.noreply.github.com>
2022-05-04 10:34:05 -0300

    Limpieza de lineas Se eliminaron líneas en el archivo ts. las cuales no se utilizaban Se elimino un titulo no utilizado en html

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 let btnCalcular = document.getElementById("enviarArea");
-let dato1 = document.getElementById("dato1");
-let dato2 = document.getElementById("dato2");
-//let base: number = Number(dato1.value);
-//let altura: number = Number(dato2.value);
-//let area: number = Number(dato1.value*dato2.value);
 btnCalcular.addEventListener("click", () => {
-  console.log("La base mide", dato1.value);
-  console.log("la altura mide", dato2.value);
-  console.log(" EL AREA DEL RECTANGULO ES: ", dato1.value * dato2.value);
+  let base: number = Number(dato1.value);
+  let altura: number = Number(dato2.value);
+  let area: number = Number(base * altura);
+  console.log("La base mide", base);
+  console.log("la altura mide", altura);
+  console.log(" EL AREA DEL RECTANGULO ES: ", area);
 });
